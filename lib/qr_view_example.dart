ee67738c1fe5ebedfcc99648cfe9f975d32c4613
Ayoub <azaydanaitzaid@ext.aragon.es>
2025-04-24 08:27:57 +0200

    io

--- lib/qr_view_example.dart
+++ lib/qr_view_example.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart'; 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/FormPage.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
